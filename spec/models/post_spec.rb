f426e6a0e08555e364c9bd5caee7954745e5f8b9
Deepak Mahakale <deepakmahakale@gmail.com>
2016-06-14 11:47:04 +0530

    Add test case for Post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-
-  it 'returns true' do
-    expect(2 + 2).to eq(4)
+  context 'Model validations' do
+    it 'validates presence of title' do
+      post = Post.new
+      post.valid?
+      expect(post.errors).to have_key(:title)
+    end
   end
 end
